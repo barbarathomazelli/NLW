76768cc96b5c4ec1cba2c61b49bd41bbb28e1bdf
barbarathomazelli <barbarathomazelli@gmail.com>
2023-01-22 17:58:43 -0300

    add

--- scripit.js
+++ scripit.js
@@ -1,1 +1,13 @@
-alert("hello")
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+    run:['01-01','01-02','01-06'],
+    takePill:['01-03'],
+    school:['01-02'],
+    water:['01-04,01-05'],
+    food:['01-07,01-08,01-09'],
+}
+
+nlwSetup.setData(data)
+nlwSetup.loud()
